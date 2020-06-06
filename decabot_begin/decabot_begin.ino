f114666833affe28c34bb7fd93130861d7c3d074
triwaca <daniel.helptech@gmail.com>
2020-06-06 17:46:14 -0300

    Decabot object turning off motors, making basic sounds

--- decabot_begin/decabot_begin.ino
+++ decabot_begin/decabot_begin.ino
@@ -7,12 +7,65 @@
  *  
  */
 
-void setup() {
-  // put your setup code here, to run once:
+class Decabot {
+  private:
+    //private vars
+      int latchPin = 8;   //Pin connected to ST_CP of 74HC595
+      int clockPin = 7;   //Pin connected to SH_CP of 74HC595
+      int dataPin = 6;    //Pin connected to DS of 74HC595
+      int buzzerPin = 3;  //Pin connected to buzzer
+      int ledPin = 2;     //Pin connected to frontal LED
+  public:
+    //constructor
+    Decabot(int nothing) {
+      //define pin modes for shift register, buzzer and LED
+      pinMode(buzzerPin, OUTPUT);
+      pinMode(ledPin, OUTPUT);
+      pinMode(latchPin, OUTPUT);
+      pinMode(clockPin, OUTPUT);
+      pinMode(dataPin, OUTPUT);
+    }
 
+    void resetMotor() {
+      digitalWrite(latchPin, LOW);
+      shiftOut(dataPin, clockPin, MSBFIRST, B00000000); //Send all off to 74HC595
+      digitalWrite(latchPin, HIGH);
+    }
+
+    void soundBegin() {
+      digitalWrite(ledPin, HIGH);
+      for(int i=400;i<1000;i++){
+        tone(buzzerPin, i, 3);
+        delay(3);
+      }
+      noTone(buzzerPin);
+      delay(50);
+      this->beep(50);
+      delay(50);
+      this->beep(50);
+      digitalWrite(ledPin, LOW);
+    }
+
+    void beep(int time){
+      tone(buzzerPin, 1000, 50);
+      delay(time);
+      noTone(buzzerPin);
+      delay(time);
+    }
+
+    void boot(){
+      //Boot sequence
+      this->soundBegin();
+    }
+    
+};
+
+Decabot robot(1); //create object Decabot
+
+void setup() {
+  robot.boot();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(1000);
 }
