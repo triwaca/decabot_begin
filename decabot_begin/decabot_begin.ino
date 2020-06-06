c08893cb733c1d540e909cf680c2bbf52cd45053
triwaca <daniel.helptech@gmail.com>
2020-06-06 16:37:52 -0300

    1st blank code

---
+++ decabot_begin/decabot_begin.ino
@@ -0,0 +1,18 @@
+/*
+ *  Decabot 0.01
+ *  
+ *  Codes for creation of basic Decabot Lib with step motors
+ *  
+ *  Daniel Chagas
+ *  
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
