305fb180587b6a914eeaaf81e8a509e7080db387
triwaca <daniel.helptech@gmail.com>
2020-06-06 19:50:54 -0300

    Some new sounds, name and owner functions

--- decabot_begin/decabot_begin.ino
+++ decabot_begin/decabot_begin.ino
@@ -15,6 +15,9 @@ class Decabot {
       #define dataPin   6 //Pin connected to DS of 74HC595
       #define buzzerPin 3 //Pin connected to buzzer
       #define ledPin    2 //Pin connected to frontal LED
+      #include <EEPROM.h>
+      char decabotName[5] = "A01  ";
+      char decabotOwner[50] = "anybody@decabot.com";
   public:
     //constructor
     Decabot(int serialSpeed) {
@@ -54,15 +57,64 @@ class Decabot {
       delay(time);
     }
 
+    void recordingSound() {
+      //sound played everytime something is recorded in EEPROM
+      for(int x=0;x<3;x++){
+        this->beep(50);
+        delay(50);
+      }
+    }
+
     void boot(){
       //Boot sequence
       this->output(F("Initializing Decabot..."));
       this->resetMotor();
       this->soundBegin();
+      this->whoami();
       this->output(F("READY!"));
     }
 
+    void whoami() {
+      //OUtputs tag name and owner e-mail of Decabot
+      //UniqueIDdump(Serial);
+      String tmp1 = F("Decabot Name: ");
+      for(int i=896;i<=900;i++){
+        tmp1.concat((char) EEPROM.read(i));
+        decabotName[i - 896] = EEPROM.read(i);
+      }
+      this->output(tmp1);
+      String tmp2 = F("Owner: ");
+      for(int i=901;i<950;i++){
+        tmp2.concat((char) EEPROM.read(i));
+      }
+      this->output(tmp2);
+    }
+
+    void yourNameIs(String parameter){
+      //change Decabot's tag name on EEPROM
+      this->recordingSound();
+      parameter.toCharArray(decabotName,6);
+      for(int i=0;i<=5;i++){
+        EEPROM.write(i + 896,decabotName[i]);
+      }
+      this->output(F("new name:"));
+      this->output(decabotName);
+    }
+
+    void yourOwnerIs(String parameter){
+      //change decabot emails owner on EEPROM
+      this->recordingSound();
+      parameter.toCharArray(decabotOwner,50);
+      for(int i=0;i<=50;i++){
+        EEPROM.write(i + 902,decabotOwner[i]);
+      }
+      this->output(F("Decabot owner: "));
+      this->output(decabotOwner);
+    }
+
     void output(String message){
+      //Terminal-like output, with timestamp
+      //User must start serial in setup() to work
       String msg = "";
       msg.concat(String(millis()/1000.0,2));
       msg.concat(F(">"));
@@ -80,6 +132,6 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
+  delay(5000);
   robot.output("ok");
 }
