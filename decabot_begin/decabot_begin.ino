26d527f66d66363f9e35990bb81f8f3f8ffff296
triwaca <daniel.helptech@gmail.com>
2020-06-06 18:58:48 -0300

    Output, boot function

--- decabot_begin/decabot_begin.ino
+++ decabot_begin/decabot_begin.ino
@@ -10,14 +10,14 @@
 class Decabot {
   private:
     //private vars
-      int latchPin = 8;   //Pin connected to ST_CP of 74HC595
-      int clockPin = 7;   //Pin connected to SH_CP of 74HC595
-      int dataPin = 6;    //Pin connected to DS of 74HC595
-      int buzzerPin = 3;  //Pin connected to buzzer
-      int ledPin = 2;     //Pin connected to frontal LED
+      #define latchPin  8 //Pin connected to ST_CP of 74HC595
+      #define clockPin  7 //Pin connected to SH_CP of 74HC595
+      #define dataPin   6 //Pin connected to DS of 74HC595
+      #define buzzerPin 3 //Pin connected to buzzer
+      #define ledPin    2 //Pin connected to frontal LED
   public:
     //constructor
-    Decabot(int nothing) {
+    Decabot(int serialSpeed) {
       //define pin modes for shift register, buzzer and LED
       pinMode(buzzerPin, OUTPUT);
       pinMode(ledPin, OUTPUT);
@@ -30,6 +30,7 @@ class Decabot {
       digitalWrite(latchPin, LOW);
       shiftOut(dataPin, clockPin, MSBFIRST, B00000000); //Send all off to 74HC595
       digitalWrite(latchPin, HIGH);
+      this->output(F("Reset motors"));
     }
 
     void soundBegin() {
@@ -55,17 +56,30 @@ class Decabot {
 
     void boot(){
       //Boot sequence
+      this->output(F("Initializing Decabot..."));
+      this->resetMotor();
       this->soundBegin();
+      this->output(F("READY!"));
+    }
+
+    void output(String message){
+      String msg = "";
+      msg.concat(String(millis()/1000.0,2));
+      msg.concat(F(">"));
+      msg.concat(message);
+      Serial.println(msg);
     }
     
 };
 
-Decabot robot(1); //create object Decabot
+Decabot robot(9600); //create object Decabot
 
 void setup() {
+  Serial.begin(9600);
   robot.boot();
 }
 
 void loop() {
   delay(1000);
+  robot.output("ok");
 }
